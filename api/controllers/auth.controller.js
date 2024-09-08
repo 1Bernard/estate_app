a5034155a173da72a8eb7c8cb3931c9ca9c339e8
Bernard Owusu Ansah <90481075+1Bernard@users.noreply.github.com>
2024-09-08 08:44:12 +0000

    configure prisma with mongo db

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,17 @@
-export const register = (req, res) => {
+import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
+
+export const register = async (req, res) => {
   const { name, email, password } = req.body
+
+  //HASH PASSWORD
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password: ", hashedPassword)
+
+  //CREATE NEW USER AND SAVE TO DB
+  const newUSer = await prisma.user.create({
+    
+  })
   
 }
 export const login = (req, res) => {
